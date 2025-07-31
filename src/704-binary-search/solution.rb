1fd60d8e91d6ee3694c63f0d400308d20f5edeef
Joyvis Santana <jjs276@gmail.com>
2025-07-31 19:54:26 -0300

    includes recursive solution for 704

--- src/704-binary-search/solution.rb
+++ src/704-binary-search/solution.rb
@@ -11,3 +11,22 @@ def search(nums, target)
   end
   -1
 end
+
+# recursive solution
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer}
+def search(nums, target, l = 0, r = nums.length)
+  mid = (l + r).div(2)
+  return -1 unless l <= r && mid < nums.length
+
+  if target == nums[mid]
+      return mid
+  elsif target < nums[mid]
+      r = mid - 1
+  else
+      l = mid + 1
+  end
+
+  search(nums, target, l, r)
+end
