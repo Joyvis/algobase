1346c4ca13c0154d3fa7ac40a8bbd5874894d2a4
Joyvis Santana <jjs276@gmail.com>
2025-08-26 12:56:11 -0300

    feat(235): implements recursive version

--- src/235-lowest-common-ancestor-of-a-binary-search-tree/solution.rb
+++ src/235-lowest-common-ancestor-of-a-binary-search-tree/solution.rb
@@ -24,3 +24,16 @@ def lowest_common_ancestor(root, p, q)
 
   dfs.call(root)
 end
+
+# Recursive solution
+def lowest_common_ancestor(root, p, q)
+  return unless root
+
+  if root.val < p.val && root.val < q.val
+    lowest_common_ancestor(root.right, p, q)
+  elsif root.val > p.val && root.val > q.val
+    lowest_common_ancestor(root.left, p, q)
+  else
+    root
+  end
+end
