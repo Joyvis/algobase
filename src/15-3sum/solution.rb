9c84c66475fb627ab0a3041a0d8f4b0dec7706a5
Joyvis Santana <jjs276@gmail.com>
2026-06-08 11:53:47 -0300

    refact(15-3_sum): improve performance

--- src/15-3sum/solution.rb
+++ src/15-3sum/solution.rb
@@ -1,31 +1,29 @@
 # @param {Integer[]} nums
 # @return {Integer[][]}
 def three_sum(nums)
-    nums.sort!
-    res = [] 
-    nums.each_with_index do |num, i|
-        next if i > 0 && num == nums[i-1]
+  nums.sort!
+  res = []
+  nums.each_with_index do |num, i|
+    break if num > 0
+    next if i > 0 && num == nums[i - 1]
 
-        l, r = i + 1, nums.length - 1
+    l, r = i + 1, nums.length - 1
+    while l < r
+      sum = num + nums[l] + nums[r]
+      if sum.zero?
+        res << [num, nums[l], nums[r]]
+        l += 1
+        r -= 1
 
-        while l < r
-            values = [num, nums[l], nums[r]]
-            sum = values.sum
-            if sum == 0
-                res << values
-
-                l += 1
-                r -= 1
-
-                l += 1 while l < r && nums[l-1] == nums[l]
-                r -= 1 while l < r && nums[r+1] == nums[r]
-            elsif sum < 0
-                l += 1
-            else
-                r -= 1
-            end
-        end
+        l += 1 while l < r && nums[l] == nums[l - 1]
+        r -= 1 while l < r && nums[r] == nums[r + 1]
+      elsif sum > 0
+        r -= 1
+      else
+        l += 1
+      end
     end
+  end
 
-    res
+  res
 end
